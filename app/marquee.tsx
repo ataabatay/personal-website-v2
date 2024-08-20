cb6629c0856120bc3edcd204ec4a270afbe14555
ataabatay <aliataabatay@gmail.com>
2024-08-20 17:03:29 +0100

    tried to fix the projects marquee but failed

--- app/marquee.tsx
+++ app/marquee.tsx
@@ -2,28 +2,32 @@ import { cn } from '@/lib/utils';
 import Marquee from '@/components/magicui/marquee';
 import { MagicCard } from '@/components/magicui/magic-card';
 
-export function MarqueeCard({ item }) {
+export function MarqueeNavCard({ navItem }) {
   return (
     <MagicCard
-      className={`cursor-pointer flex flex-col p-4 justify-center bg-transparent  text-center ${item.style} whitespace-nowrap w-32 h-32 font-light tracking-wider text-lg hover:font-semibold hover:scale-110 transform transition duration-75 border-transparent rounded-none text-white`}
-      gradientColor='rgb(30 41 59)'
+      className={`cursor-pointer flex flex-col p-4 justify-center bg-transparent  text-center ${navItem.style} whitespace-nowrap w-32 h-32 font-light tracking-wider text-md hover:font-semibold hover:scale-110 transform transition duration-75 text-white`}
+      gradientColor="rgb(30 41 59)"
     >
-      {item.img ? 
-      <img src={item.img} className='grayscale hover:grayscale-0'></img>  
-      : 
-      <p className='size-full rounded-none'>{item.name}</p>
-      }
+      <p>{navItem.name}</p>
     </MagicCard>
   );
 }
 
-export function MarqueeForward({items}) {
+export function MarqueeProjectCard({ projectItem }) {
+  return (
+    <MagicCard
+      className={`cursor-pointer flex flex-col p-4 justify-center bg-transparent  text-center ${projectItem.style} whitespace-nowrap w-32 h-32 font-light tracking-wider text-md text-white hover:relative hover:w-full hover:z-50 hover:scale-110 transform transition duration-300 grayscale hover:grayscale-0 hover:bg-slate-500"`}
+    >
+      <img src={projectItem.img}></img>
+    </MagicCard>
+  );
+}
+
+export function MarqueeForward({ items }) {
   return (
     <div className="relative flex max-w-screen flex-col items-center justify-center overflow-hidden">
       <Marquee pauseOnHover className="[--duration:30s]">
-        {items.map((item) => (
-          <MarqueeCard key={item.id} item={item} />
-        ))}
+        {items.map((item) => (item.type === 'nav' ? <MarqueeNavCard navItem={item} /> : <></>))}
       </Marquee>
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/12 bg-gradient-to-r from-black"></div>
       <div className="pointer-events-none absolute inset-y-0 right-0 w-1/12 bg-gradient-to-l from-black"></div>
@@ -31,13 +35,11 @@ export function MarqueeForward({items}) {
   );
 }
 
-export function MarqueeBackward({items}) {
+export function MarqueeBackward({ items }) {
   return (
     <div className="relative flex max-w-screen flex-col items-center justify-center overflow-hidden">
       <Marquee reverse pauseOnHover className="[--duration:30s]">
-        {items.map((item) => (
-          <MarqueeCard key={item.id} item={item}/>
-        ))}
+        {items.map((item) => (item.type === 'project' ? <MarqueeProjectCard projectItem={item} /> : <></>))}
       </Marquee>
       <div className="pointer-events-none absolute inset-y-0 left-0 w-1/12 bg-gradient-to-r from-black"></div>
       <div className="pointer-events-none absolute inset-y-0 right-0 w-1/12 bg-gradient-to-l from-black"></div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,35 @@
 import { MarqueeBackward, MarqueeForward } from './marquee';
 import AnimatedGridBackground from './ui/animatedGridBackground';
 
-const mainPagesArray = [
-  { id: 1, name: 'about', style: 'bg-transparent'},
-  { id: 1, name: 'projects', style: 'bg-transparent'},
-  { id: 1, name: 'past', style: 'bg-transparent'},
-  { id: 1, name: 'contact', style: 'bg-transparent' },
+const LinksArray = [
+  { id: 1, type: 'nav', name: 'about' },
+  { id: 2, type: 'nav', name: 'experience' },
+  { id: 3, type: 'nav', name: 'contact' },
+  { id: 4, type: 'project', name: 'project1', img: 'https://picsum.photos/id/22/200' },
+  { id: 5, type: 'project', name: 'project2', img: 'https://picsum.photos/id/23/200' },
+  { id: 6, type: 'project', name: 'project3', img: 'https://picsum.photos/id/24/200' },
+  { id: 7, type: 'project', name: 'project4', img: 'https://picsum.photos/id/25/200' },
+  { id: 8, type: 'project', name: 'project5', img: 'https://picsum.photos/id/26/200' },
+  { id: 9, type: 'project', name: 'project6', img: 'https://picsum.photos/id/27/200' },
 ];
-
-const projectsArray = [
-  { id: 1, name: 'project1', style: 'white', img: 'https://picsum.photos/id/22/200' },
-  { id: 2, name: 'project2', style: 'white', img: 'https://picsum.photos/id/23/200' },
-  { id: 3, name: 'project3', style: 'white', img: 'https://picsum.photos/id/24/200' },
-  { id: 4, name: 'project4', style: 'white', img: 'https://picsum.photos/id/25/200' },
-  { id: 5, name: 'project5', style: 'white', img: 'https://picsum.photos/id/26/200' },
-  { id: 6, name: 'project6', style: 'white', img: 'https://picsum.photos/id/27/200' },
-]
 
 export default function Home() {
   return (
     <>
-      <AnimatedGridBackground/>
+      <AnimatedGridBackground />
       <main className="max-w-screen min-h-screen flex flex-col justify-center items-center text-light-text">
         <div className="overflow-hidden w-screen sm:max-w-screen-md">
-          <MarqueeForward items={mainPagesArray}/>
+          <MarqueeForward items={LinksArray} />
         </div>
         <div className="overflow-hidden w-screen sm:max-w-screen-md -z-50 opacity-15">
-          <MarqueeBackward items={projectsArray}/>
+          <MarqueeBackward items={LinksArray} />
         </div>
         <div className="overflow-hidden w-screen sm:max-w-screen-md -z-50 opacity-15">
-          <MarqueeForward items={mainPagesArray}/>
+          <MarqueeForward items={LinksArray} />
         </div>
         <div className="overflow-hidden w-screen sm:max-w-screen-md">
-          <MarqueeBackward items={projectsArray}/>
+          <p className="text-white py-2">Projects</p>
+          <MarqueeBackward items={LinksArray} />
         </div>
       </main>
     </>
